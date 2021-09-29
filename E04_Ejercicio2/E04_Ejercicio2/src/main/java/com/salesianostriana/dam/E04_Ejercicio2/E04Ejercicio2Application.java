82eb1345d8d1df8bd985b6168a9eeab8e39c6347
ardana.mujua21 <cardamu02@gmail.com>
2021-09-29 12:50:05 +0200

    Creacion Ejercicio2

---
+++ E04_Ejercicio2/E04_Ejercicio2/src/main/java/com/salesianostriana/dam/E04_Ejercicio2/E04Ejercicio2Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.E04_Ejercicio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E04Ejercicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E04Ejercicio2Application.class, args);
+	}
+
+}
---
+++ E04_Ejercicio2/E04_Ejercicio2/src/test/java/com/salesianostriana/dam/E04_Ejercicio2/E04Ejercicio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.E04_Ejercicio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E04Ejercicio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
