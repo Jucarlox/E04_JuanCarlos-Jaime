9f45db9845eb56866e6c85568c3ab2d4e362ca4d
jotaerre123 <jaimejimenezrios10@gmail.com>
2021-09-29 12:51:01 +0200

    Añadido Ej1

---
+++ E04_Ejercicio1/E04_Ejercicio1/src/main/java/com/salsianostriana/dam/E04_Ejercicio1/E04Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.salsianostriana.dam.E04_Ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E04Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E04Ejercicio1Application.class, args);
+	}
+
+}
